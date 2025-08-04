7afdc6e9bca4996b44d8f362b360a3775d481b36
sikandarsayyad <sayyadsikandar475@gmail.com>
2025-08-04 23:10:33 +0530

    App js updated

--- src/App.jsx
+++ src/App.jsx
@@ -14,6 +14,7 @@ function App() {
       <BrowserRouter>
         <Routes>
           <Route path="/" element={<Login />} />
+          <Route path="/login" element={<Login />} />
           <Route path="/dashboard" element={<Dashboard />} />
           <Route path="/forget-password" element={<ForgotPasswordForm />} />
           <Route path="/reset-password" element={<ResetPasswordForm />} />
